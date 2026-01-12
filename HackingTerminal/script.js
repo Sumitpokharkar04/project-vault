7ba874bbfab39c61bf8df3cb8e5ddf00ecdb01b3
Sumitpokharkar04 <pokharkarsumit04@gmail.com>
2026-01-12 19:35:03 +0530

    learned and implemented async await

--- HackingTerminal/script.js
+++ HackingTerminal/script.js
@@ -1,55 +1,81 @@
-async function main(){
+btn.addEventListener("click", ()=>{
 
-async function initializeTerminal() 
-{
-   return new Promise( setInterval(() =>{
-        document.querySelector('.container').innerText = "Hacking Terminal Initialized...";
-    } ,Math.floor(Math.random() * 6001) + 1000
-));
-}
 
-async function startHacking()
-{
-    setTimeout(() => {
-        document.querySelector('.container').innerText = "Hacking in Progress...";
-},Math.floor(Math.random() * 6001) + 1000);
-}
+ async function main() {
 
-async function bypassSecurity()
-{
-    setTimeout(() => {
-        document.querySelector('.container').innerText = "Bypassing firewall...";
-},Math.floor(Math.random() * 6001) + 1000);
-}
+    async function initializeTerminal() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                document.querySelector('.box').innerText = "Hacking Terminal Initialized...";
+                resolve();
+            }, Math.floor(Math.random() * 6001) + 1000);
 
-async function searchPasswords()
-{
-    setTimeout(() => {
-        document.querySelector('.container').innerText = "Searching for passwords...";
-},Math.floor(Math.random() * 6001) + 1000);
-}
+        });
+    }
 
-async function extractData()
-{
-    setTimeout(() => {
-        document.querySelector('.container').innerText = "Extracting data...";
-},Math.floor(Math.random() * 6001) + 1000);
-}
 
-async function completeExtraction()
-{
-    setTimeout(() => {
-        document.querySelector('.container').innerText = "Extraction completed";
-},Math.floor(Math.random() * 6001) + 1000);
-}
+    async function startHacking() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                document.querySelector('.box').innerText = "Hacking in Progress...";
+                resolve();
+            }, Math.floor(Math.random() * 6001) + 1000);
+        });
+    }
 
-initializeTerminal();
-startHacking();
-bypassSecurity();
-searchPasswords();
-extractData();
-completeExtraction();
+
+    async function bypassSecurity() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                document.querySelector('.box').innerText = "Bypassing firewall...";
+                resolve();
+            }, Math.floor(Math.random() * 6001) + 1000);
+
+        });
+    }
+
+    async function searchPasswords() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                document.querySelector('.box').innerText = "Searching for passwords...";
+                resolve();
+            }, Math.floor(Math.random() * 6001) + 1000);
+
+
+        });
+    }
+
+    async function extractData() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                document.querySelector('.box').innerText = "Extracting data...";
+                resolve();
+            }, Math.floor(Math.random() * 6001) + 1000);
+
+        });
+    }
+
+    async function completeExtraction() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                document.querySelector('.box').innerText = "Extraction completed";
+                resolve();
+            }, Math.floor(Math.random() * 6001) + 1000);
+
+        })
+    }
+
+    await initializeTerminal();
+    await startHacking();
+    await bypassSecurity();
+    await searchPasswords();
+    await extractData();
+    await completeExtraction();
 
 }
 
 main();
+
+click();
+})
+
